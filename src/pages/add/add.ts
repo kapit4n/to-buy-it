93bd3a5c5e7092a71ad4b46cab976fbf02d0a501
larce <luis.arce22@gmail.com>
2017-06-19 22:26:02 -0400

    Updated add page to save data using todoBuyService

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
+import { TodoBuyServiceProvider } from '../../providers/todo-buy-service/todo-buy-service';
 
 /**
  * Generated class for the AddPage page.
@@ -10,10 +11,20 @@ import { NavController} from 'ionic-angular';
 @Component({
   selector: 'page-add',
   templateUrl: 'add.html',
+  providers: [ TodoBuyServiceProvider ],
 })
 export class AddPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public dataService: TodoBuyServiceProvider) {
+
+  }
+
+  save() {
+    var data = {name: "Name", imageUrl: "ImageUrl", description: "Description", price: "40000"};
+    this.dataService.save(data)
+    .then(data => {
+      console.log(data);
+    });
   }
 
 }
